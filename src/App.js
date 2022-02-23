93b93dca7bd83158c3f1aff430867a7213276c61
Javed Baloch <mjavedahmed@live.com>
2022-02-23 19:59:01 +0500

    Layout structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { Link, Route, Routes} from 'react-router-dom';
+import { Typography, Space, Layout } from 'antd'
+
+import { Navbar, Footer, Homepage, Exchanges, CryptoDetails, Cryptocurrencies } from './components'
+import './App.css'
 
 function App() {
   return (
-    <div>
-        <h1>Cryptoverse</h1>
+    <div className="app">
+        <div className="navbar">
+            <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+              <div className="routes">
+                <Routes>
+                  <Route exact element={<Homepage />} />
+                  <Route element={<Exchanges />} />
+                  <Route element={<Cryptocurrencies />} />
+                  <Route element={<CryptoDetails />} />
+                </Routes>
+              </div>
+            </Layout>
+            <div className="footer" level={5} style={{ color: 'white', textAlign: 'center' }}>
+              <Typography.Title>
+                Cryptoverse <br />
+                All rights reserverd
+              </Typography.Title>
+              <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+              </Space>
+            </div>
+        </div>
     </div>
   )
 }
